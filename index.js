f9ae59497b16c2945c4adbc073eb1b0e2f7e7cbf
Cara Kane <cara.kane@gmail.com>
2017-12-15 18:00:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  return(document.querySelector('#nested .target'))
+  return(document.querySelector("#nested .target"))
 }
 
 function deepestChild() {
+  let node = document.getElementById("#grand-node")
+  let nextNode = node.children[0]
 
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
 }
 
 function increaseRankBy(n) {
